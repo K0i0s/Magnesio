159d5563874714dc9f26d1f81eb2da075d427938
CesFran <al20760438@ite.edu.mx>
2021-12-08 15:58:05 -0800

    agregarNodo con Trabajador y pedirDatos

--- arboles/baseArboles/ArbolBinario.java
+++ arboles/baseArboles/ArbolBinario.java
@@ -77,4 +77,100 @@ public class ArbolBinario{
         }
     }
 
+    public void agregarNodo(Trabajador num){
+        NodoArbol nuevo = new NodoArbol(num);
+        if(raiz == null){
+            raiz = nuevo;
+        }
+        else{
+            NodoArbol aux = raiz;
+            NodoArbol padre;
+            while(true){
+                padre = aux;
+                if(num.id < aux.dato.id){
+                    aux = aux.izq;
+                    if (aux == null) {
+                        padre.izq = nuevo;
+                        System.out.println("Se ha agregado exitosamente.");
+                        nElementos++;
+                        return;
+                    }
+                }
+                else{
+                    aux = aux.der;
+                    if(aux == null){
+                        padre.der = nuevo;
+                        System.out.println("Se ha agregado exitosamente.");
+                        nElementos++;
+                        return;
+                    }
+                }
+            }
+        }        
+    }
+
+    public int contador() {
+        return nElementos;
+    }
+
+    public Trabajador pedirDatos() throws IOException {
+        BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
+        String nombre;
+        String apellidoP;
+        String apellidoM;
+        int edad = 0;
+        String genero;
+        int id = 0;
+        double sueldo = 0;
+        String puesto;
+        boolean esNum;
+        esNum = false;
+        id = (int) (Math.random() * 9000 + 1000);
+        while (buscarNodo(id) != null) {
+            id = (int) (Math.random() * 1000 + 1);
+        }
+        System.out.println("La ID es: " + id);
+
+        System.out.println("\nNombre: ");
+        nombre = lector.readLine();
+
+        System.out.println("\nApellido Paterno: ");
+        apellidoP = lector.readLine();
+
+        System.out.println("\nApellido Materno: ");
+        apellidoM = lector.readLine();
+
+        esNum = false;
+        while (esNum == false) {
+            try {
+                System.out.println("\nEdad: ");
+                edad = Integer.parseInt(lector.readLine());
+                esNum = true;
+            } catch (NumberFormatException e) {
+                System.out.println("\nEntrada no válida...");
+                esNum = false;
+            }
+        }
+
+        System.out.println("\nGénero: ");
+        genero = lector.readLine();
+
+        System.out.println("\nPuesto: ");
+        puesto = lector.readLine();
+
+        esNum = false;
+        while (esNum == false) {
+            try {
+                System.out.println("\nSueldo: ");
+                sueldo = Double.parseDouble(lector.readLine());
+                esNum = true;
+            } catch (NumberFormatException e) {
+                System.out.println("\nEntrada no válida...");
+                esNum = false;
+            }
+        }
+
+        Trabajador aux = new Trabajador(nombre, apellidoP, apellidoM, edad, genero, id, sueldo, puesto);
+        return aux;
+    }
 } 
