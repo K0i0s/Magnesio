5b4990900be91dc8284f29f68158ecb8d049ae1a
RICARDO ESCOBAR CESEÑA <al20760258@ite.edu.mx>
2021-12-08 15:53:12 -0800

    main

--- arboles/baseArboles/ArbolBinario.java
+++ arboles/baseArboles/ArbolBinario.java
@@ -1,6 +1,48 @@
 package arboles.baseArboles;
 
+import arboles.trabajadores.Trabajador;
+
 public class ArbolBinario{
     
+    public NodoArbol raiz;
+    private static int nElementos = 0;
+    
+    public ArbolBinario(){ 
+        raiz = null; 
+    } 
+
+    public ArbolBinario(NodoArbol raiz){ 
+        this.raiz = raiz; 
+    } 
+
+    public NodoArbol raizArbol(){ 
+        return raiz; 
+    } 
+
+    boolean esVacio(){ 
+        return raiz == null; 
+    } 
+
+    public static NodoArbol nuevoArbol(NodoArbol ramaIzdo, Trabajador valor, NodoArbol ramaDcho){ 
+        return new NodoArbol(ramaIzdo, valor, ramaDcho); 
+    }
+
+    public NodoArbol buscarNodo(int d){
+        NodoArbol aux = raiz;
+        if(aux == null){
+            return null;
+        }
+        else{
+            while (aux != null && aux.dato.id != d) {
+                if(d < aux.dato.id){
+                    aux = aux.izq;
+                }
+                else{
+                    aux = aux.der;
+                }
+            }
+        }
+        return aux;
+    }
     
 } 
